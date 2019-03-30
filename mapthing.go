1f9e3539af43879db8ac890cc7e9b705ed32b7c4
Bracken Dawson <abdawson@gmail.com>
2019-03-30 21:43:03 +0000

    Make it load the index and not list dirs

--- mapthing.go
+++ mapthing.go
@@ -3,9 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("assets")))
+	http.Handle("/", http.FileServer(noListFS{http.Dir("webapp")}))
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+type noListFS struct {
+	fs http.FileSystem
+}
+
+func (n noListFS) Open(path string) (http.File, error) {
+	f, err := n.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := n.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
